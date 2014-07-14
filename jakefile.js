38d2740ecac2ca2731be96074a3e66c0dbbc874f
abhiaiyer91 <abhiaiyer91@gmail.com>
2014-07-13 20:49:14 -0700

    Lint Strict

--- jakefile.js
+++ jakefile.js
@@ -1,6 +1,37 @@
-task("default", ["lint"]);
-desc("lint everything");
-task("lint", [], function(){
-    var lint = require("./build/lint/lint.js");
-    lint.validateFile("jakefile.js", {}, {});
-});
+/*global desc, task, jake, fail, complete */
+(function() {
+    "use strict";
+
+    task("default", ["lint"]);
+
+    desc("Lint everything");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint.js");
+
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        lint.validateFileList(files.toArray(), options, {});
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise:true,
+            curly:false,
+            eqeqeq:true,
+            forin:true,
+            immed:true,
+            latedef:true,
+            newcap:true,
+            noarg:true,
+            noempty:true,
+            nonew:true,
+            regexp:true,
+            undef:true,
+            strict:true,
+            trailing:true,
+            node:true
+        };
+    }
+}());
